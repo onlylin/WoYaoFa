8a685bd299d521c240816f6f9580a6642e95debb
林洁 <linjie@linjiedembp.lan>
2015-12-04 15:44:22 +0800

    Initial Commit

---
+++ WoYaoFa/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  WoYaoFa
+//
+//  Created by 林洁 on 15/12/4.
+//  Copyright © 2015年 Lin. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WoYaoFaTests/WoYaoFaTests.m
@@ -0,0 +1,39 @@
+//
+//  WoYaoFaTests.m
+//  WoYaoFaTests
+//
+//  Created by 林洁 on 15/12/4.
+//  Copyright © 2015年 Lin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WoYaoFaTests : XCTestCase
+
+@end
+
+@implementation WoYaoFaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
